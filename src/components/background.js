ce7f836936c0165cae9ceb1fe054d31514639808
Valerij1 <valerij.semikolenov@gmail.com>
2026-09-22 23:24:10 +0300

    Настройка стиля подложки

--- src/components/background.js
+++ src/components/background.js
@@ -3,10 +3,16 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Background({ children }) {
   return (
-    <div style={{ position: 'relative', width: '100%', minHeight: '650px', borderRadius: '8px', overflow: 'hidden' }}>
-      <img src={useBaseUrl('/img/bg.png')} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', objectFit: 'cover', zIndex: 1 }} />
-      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(255,255,255,0.70)', zIndex: 2 }} />
-      <div style={{ position: 'relative', zIndex: 3, padding: '40px' }}>
+    <div className="background-container">
+      <img
+        src={useBaseUrl('/img/bg.png')}
+        className="background-image"
+        alt=""
+      />
+
+      <div className="background-overlay" />
+
+      <div className="background-content">
         {children}
       </div>
     </div>
